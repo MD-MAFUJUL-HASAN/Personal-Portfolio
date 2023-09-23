0e51c4605b049aae5d21d4e722c264252b3d5ae7
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-09-23 12:06:19 +0600

    Update CertificatePage.js

--- src/pages/certificate_page/CertificatePage.js
+++ src/pages/certificate_page/CertificatePage.js
@@ -507,6 +507,58 @@ export default function CertificatePage() {
                             </div>
                           </Fade>
                         </Col>
+                        <Col md={4}>
+                          <Fade bottom duration={2000} distance="20px">
+                            <div className="cert-card">
+                              <div className="content">
+                                <a
+                                  href="https://skill.jobs/career-doctor-report/?u=f5eb57cc-6b6b-4277-a765-e5df63296aed"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  <div className="content-overlay"></div>
+                                  <div
+                                    className="cert-header"
+                                    style={{ backgroundColor: "white" }}
+                                  >
+                                    {
+                                      <img
+                                        className="logo_img"
+                                        src={skill}
+                                        alt={skill}
+                                      />
+                                    }
+                                  </div>
+                                  <div className="content-details fadeIn-top">
+                                    <h3
+                                      className="content-title"
+                                      style={{ color: "black" }}
+                                    >
+                                      Certificate
+                                    </h3>
+                                  </div>
+                                </a>
+                              </div>
+                              <div className="cert-body">
+                                <h2
+                                  className="cert-body-title"
+                                  style={{ fontWeight: 700, color: "#fbd9ad" }}
+                                >
+                                  Career Doctor Report.
+                                </h2>
+                                <h3
+                                  className="cert-body-subtitle"
+                                  style={{
+                                    color: "#eb90ff",
+                                    marginBottom: "0px",
+                                  }}
+                                >
+                                  -Skill Jobs.
+                                </h3>
+                              </div>
+                            </div>
+                          </Fade>
+                        </Col>
                       </Row>
                     </Col>
                   </Row>
