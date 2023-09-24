36359378bc9277270762f0850b547f40c9fc64ba
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-09-24 18:11:58 +0600

    Update Skills.js

--- src/components/aboutme/skills/Skills.js
+++ src/components/aboutme/skills/Skills.js
@@ -1,17 +1,17 @@
-import React from 'react'
-// import { Button } from 'react-bootstrap';
-import { Routes,Route,Link } from 'react-router-dom';
-import PersonalSkill from './PersonalSkill'
-import TechnicalSkill from './TechnicalSkill'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Skills() {
   return (
     <div>
-        <div className="mt-4 d-flex flex-row justify-content-center">
-            <Link className="btn btn-primary me-3" to="/personalskill">Personal Skill</Link>
-            <Link className="btn btn-primary" to="/technicalSkill">Technical Skill</Link>
-        </div>
+      <div className="mt-4 d-flex flex-row justify-content-center">
+        <Link className="btn btn-primary me-3" to="/personalskill">
+          Personal Skill
+        </Link>
+        <Link className="btn btn-primary" to="/technicalSkill">
+          Technical Skill
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
