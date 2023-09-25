09f341709365231a16072344da264ff1e604274c
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-09-25 14:12:32 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,59 @@
 // backend/server.js
+
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000; // Choose a port for your backend
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/contactusdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Define a route to handle form submissions
-app.post("/api/submitForm", (req, res) => {
-  const formData = req.body;
+// Define the Schema for your contact form data
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+});
 
-  // Handle the form data (e.g., save to a database)
-  console.log("Received form data:", formData);
+const Contact = mongoose.model("Contact", contactSchema);
 
-  // Respond with a success message
-  res.json({ message: "Form data received successfully!" });
+// Create a route to handle form submissions
+app.post("/api/submitForm", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create a new Contact document
+  const newContact = new Contact({
+    name,
+    email,
+    message,
+  });
+
+  try {
+    await newContact.save(); // Use await to save the document
+    res.json({ message: "Form submitted successfully" });
+  } catch (error) {
+    console.error("Error saving contact form data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
