85019e0b20d8255027a9270e5ec0b55a185698b2
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-10-02 13:59:42 +0600

    Update About.js

--- src/components/aboutme/about/About.js
+++ src/components/aboutme/about/About.js
@@ -29,42 +29,35 @@ const About = () => {
             <Fade duration={3000}>
               <p className="home-about-body">
                 My name is <b className="purple">MD Mafujul Hasan </b> 
-                  and I am a <b className="purple">computer science and engineering</b> 
-                    student at <b className="purple">Sonargaon University</b>
+                  and I am a <b className="purple">computer science and engineering </b> 
+                    student at <b className="purple">Sonargaon University </b>
                       in Dhaka, Bangladesh.
                 <br />
-                  I am passionate about developing computerized systems that gather, retain, evaluate and exhibit data to facilitate the achievement of organizational aims and objectives.
-                <br />I am proficient in various programming languages such as &nbsp;
-                <i>
+                <br />
+                        I am proficient in various programming languages such as &nbsp;
                   <b className="purple">
                     {" "}
                     C, C++, Java, Python, HTML, CSS, PHP, and JavaScript.{" "}
                   </b>
-                </i>
                 <br />
                 <br />
                 I also have experience working with frameworks and libraries such as &nbsp;
-                <i>
+
                   <b className="purple">
                     {" "}
                     NodeJS, ReactJS, OpenCV, Pandas, and NumPy.{" "}
                   </b>
-                </i>
                 <br />
                 <br />
                 Whenever possible, I also apply my passion for developing
                 products with &nbsp;
-                <i>
                   <b className="purple">
                     {" "}
                     Modern Javascript Library and Frameworks
                   </b>
-                </i>
                 <br />
                 <br />In my free time, I enjoy learning new technologies and &nbsp;
-                <i>
                   <b className="purple">building new web technologies and products. </b>
-                </i>
               </p>
             </Fade>
           </Col>
