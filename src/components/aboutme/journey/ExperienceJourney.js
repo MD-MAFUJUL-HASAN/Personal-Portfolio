8312925c41ba036cd6136d291c194e150f85e449
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-12-31 21:26:04 +0600

    Update ExperienceJourney.js

--- src/components/aboutme/journey/ExperienceJourney.js
+++ src/components/aboutme/journey/ExperienceJourney.js
@@ -34,7 +34,7 @@ function ExperienceJourney() {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          contentArrowStyle={{ borderLeft: "7px solid  rgb(78 22 112)" }}
           date="01/12/2023 - 05/01/2024"
           iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           icon={<PersonIcon />}
